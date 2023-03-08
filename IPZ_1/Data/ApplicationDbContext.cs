51485a94569ba24c155382d64808a4c246db7bc0
ValeraSan <yarovoy.valera2002@gmail.com>
2023-03-09 00:20:36 +0200

    connect-SQLServer-and-create-Controller

---
+++ IPZ_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IPZ_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IPZ_1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+
+    }
+}
---
+++ IPZ_1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IPZ_1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+
+
+    }
+}
