0de9ce58fe29180a279cb9e74fbdb747f6847fe5
ValeraSan <yarovoy.valera2002@gmail.com>
2023-04-04 23:57:56 +0300

    new view port

---
+++ IPZ_1/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using IPZ_1.Data;
+using Microsoft.AspNetCore.Mvc;
+using IPZ_1.Models;
+using System.Collections.Generic;
+
+namespace IPZ_1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+
+
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
